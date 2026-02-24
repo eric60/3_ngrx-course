b6d74c2bcbc4572b3b6f5d746f7acc0a3622c9a9
Eric <ericshi98@gmail.com>
2026-02-24 17:05:27 -0500

    create derived observable $login actions

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,5 +1,7 @@
 import {Inject, Injectable} from "@angular/core";
-import {act, Actions} from "@ngrx/effects";
+import {act, Actions, ofType} from "@ngrx/effects";
+import {AuthActions} from "./action-types";
+import {tap} from "rxjs/operators";
 
 // lesson 19: understanding ngrx effects - a simple example
 // recall write it down x3
@@ -10,13 +12,29 @@ import {act, Actions} from "@ngrx/effects";
 export class AuthEffects  {
 
   constructor(private $actions: Actions) {
+
+    // new way is to create a new observable that emits ONLY login actions
+    const login$ = $actions.pipe(
+      ofType(AuthActions.loginAction),
+      tap(action => {
+          localStorage.setItem('user', JSON.stringify(action.user)) // type safe so can access action.user
+      })
+    )
+
+    // subscribe or else nothing will happen
+    login$.subscribe()
+
+   /*
+    old way to manually subscribe and filter
     // there can be 10+ actions to handle for the effect
     $actions.subscribe(action => {
       if (action.type == '[Login Page] User Login') {
-          localStorage.setItem('user', JSON.stringify(action['user'])) // user profile will survive browser refreshes
+          localStorage.setItem('user', JSON.stringify(action['user']))
+          // not type safe so using brackets
+          // user profile will survive browser refreshes
         console.log("in effect from user login - saved user to storage")
       }
-    })
+    })*/
   }
 
   // as a side effect, want to save user profile to backend in addition to saving in state
