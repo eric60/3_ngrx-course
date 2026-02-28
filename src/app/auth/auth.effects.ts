49ad90ecbe6ae63a073534628e5d29f8642d23bb
Eric <ericshi98@gmail.com>
2026-02-28 11:54:07 -0500

    setup effect with createEffect

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,5 +1,5 @@
 import {Inject, Injectable} from "@angular/core";
-import {act, Actions, ofType} from "@ngrx/effects";
+import {act, Actions, createEffect, ofType} from "@ngrx/effects";
 import {AuthActions} from "./action-types";
 import {tap} from "rxjs/operators";
 
@@ -9,32 +9,47 @@ import {tap} from "rxjs/operators";
 // effect = something extra we want to do e.g. save to backend after an action has been dispatched and the reducers have updated the store
 // this class should NOT be injected anywhere in your code -- it is purely handled by the ngrx library
 @Injectable()
-export class AuthEffects  {
+// Q: angular is injectable singleton?
+// A: Yes, In Angular, a service is a singleton by default within the scope of its provider. If a service is provided at the root level of the application, there will only be one instance of that service for the entire application's lifecycle, and it is made available through Angular's dependency injection (DI) system
+// Hierarchical Injectors: Angular uses a hierarchical injection system. While providedIn: 'root' creates a single instance for the entire app, you CAN provide a service at the component or module level to create a NEW instance SPECIFIC to that injector and its children. .
+export class AuthEffects {
+
+// new way #3 is using createEffect
+  login$ = createEffect(() => {
+    return this.$actions
+      .pipe(
+        ofType(AuthActions.loginAction), // filtering for just loginAction
+        tap(action => {
+          localStorage.setItem('user', JSON.stringify(action.user));
+        })
+      );
+  }, {dispatch: false}) // very important to have config dispatch false else create infinite loop since saving storage would dispatch new action that would then trigger this effect ofType loginAction over and over again
 
   constructor(private $actions: Actions) {
 
-    // new way is to create a new observable that emits ONLY login actions
-    const login$ = $actions.pipe(
-      ofType(AuthActions.loginAction),
-      tap(action => {
+    // new way #2 is to create a new observable that emits ONLY login actions
+    /*const login$ =  this.$actions
+      .pipe(
+        ofType(AuthActions.loginAction),
+        tap(action => {
           localStorage.setItem('user', JSON.stringify(action.user)) // type safe so can access action.user
-      })
-    )
+        })
+      )*/
 
     // subscribe or else nothing will happen
-    login$.subscribe()
+    // login$.subscribe() // can remove manual subscription with createEffect(())
 
-   /*
-    old way to manually subscribe and filter
-    // there can be 10+ actions to handle for the effect
-    $actions.subscribe(action => {
-      if (action.type == '[Login Page] User Login') {
-          localStorage.setItem('user', JSON.stringify(action['user']))
-          // not type safe so using brackets
-          // user profile will survive browser refreshes
-        console.log("in effect from user login - saved user to storage")
-      }
-    })*/
+    /*
+     old way #1 to manually subscribe and filter
+     // there can be 10+ actions to handle for the effect
+     $actions.subscribe(action => {
+       if (action.type == '[Login Page] User Login') {
+           localStorage.setItem('user', JSON.stringify(action['user']))
+           // not type safe so using brackets
+           // user profile will survive browser refreshes
+         console.log("in effect from user login - saved user to storage")
+       }
+     })*/
   }
 
   // as a side effect, want to save user profile to backend in addition to saving in state
