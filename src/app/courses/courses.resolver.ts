4a64d8f1b81ddcc32d79b734d92f766496bd966e
Eric <ericshi98@gmail.com>
2026-04-17 11:20:47 -0400

    add courses.effects

--- src/app/courses/courses.resolver.ts
+++ src/app/courses/courses.resolver.ts
@@ -11,18 +11,31 @@ Are angular services singletons?
 
 Angular services are singletons by default. This means that within a typical application, Angular’s Dependency Injection (DI) system creates only one instance of a service and shares it across all components that request it.
 
-Module-level Scope: If a service is added to the providers array of a specific NgModule, it is a singleton only within the scope of that module and its children.
+Module-level Scope: If a service is added to the providers array of a specific NgModule, it is a singleton **only within the scope of that module and its children.**
 
-Component-level Instances: If you add a service to a specific component's providers array, Angular creates a **new instance** of that service for every instance of that component. This prevents it from being an app-wide singleton and is useful for **keeping state local to a component tree.**
+Component-level Instances: If you add a service to a specific component's providers array, Angular creates a **new instance** of that service **for every instance of that component**. This prevents it from being an app-wide singleton and is useful for **keeping state local to a component tree.**
+
+component-level instances
+===========================
+component instance 1 --> service instance 1
+component instance 2--> service instance 2
+
+vs
+app-level instances
+===========================
+component instance 1 --> service instance 1
+component instance 2 --> service instance 1
+
+
 
 Lazy Loading: Services provided in lazy-loaded modules can sometimes result in multiple instances if they are not correctly configured (e.g., if the same service is provided in both the root and a lazy module's providers).
 
 Summary of Scope
-Location of Provider 	Number of Instances
-@Injectable({ providedIn: 'root' })	One per application
-NgModule.providers (Root Module)	One per application
-Component.providers	One per component instance
-Lazy-loaded NgModule.providers	One per lazy-loaded module instance
+Location of Provider 	===> Number of Instances
+@Injectable({ providedIn: 'root' })	===> One per application
+NgModule.providers (Root Module)	===> One per application
+Component.providers([])	===> One per component instance
+Lazy-loaded NgModule.providers([])	===> One per lazy-loaded module instance
 
  */
 /*
@@ -53,7 +66,7 @@ export class CoursesResolver implements Resolve<any> {
 
         }),
         first(),
-        finalize(() => this.isLoading = false) // use finalize operator to ensure isLoading back to false
+        finalize(() => this.isLoading = false) // use finalize operator to ensure isLoading back to false after the observable completes
       )
 
     // Problem: 2 instances of the [Course Resolver] Load All Courses Action because with router devtools, the store is emitting multiple times during router transition. Solution: add loading flag
--- src/app/courses/home/home.component.ts
+++ src/app/courses/home/home.component.ts
@@ -34,8 +34,9 @@ export class HomeComponent implements OnInit {
     ngOnInit() {
       this.reload(); // problem: bad UX: constantly see loading screen, solution: Load just one time and fetch data from store rather than backend
       /*
-      1. 1st step in ngrx is always to define the actions first to implement handling of the course data ===> src/app/courses/courses.actions.ts
-      2. 2nd step in ngrx is
+      Step 1
+      1. 1st step in ngrx is always to define the actions first (loadAllCourses, coursesAllLoaded=createAction) to implement handling of the course data ===> src/app/courses/courses.actions.ts
+      2. 2nd step in ngrx is to define the effects ($loadCourses=createEffect)
        */
     }
 
