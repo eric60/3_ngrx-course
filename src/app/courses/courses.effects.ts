eeb68907996dfce6e78fbd8cf874ec247939ba38
Eric <ericshi98@gmail.com>
2026-04-18 10:47:22 -0400

    finish courses.effects to handle loadAllCourses action and dispatch a new action allCoursesLoaded

--- src/app/courses/courses.effects.ts
+++ src/app/courses/courses.effects.ts
@@ -1,6 +1,28 @@
 import {Inject, Injectable} from "@angular/core";
+import {CoursesHttpService} from "./services/courses-http.service";
+import {Actions, createEffect, ofType} from "@ngrx/effects";
+import {CourseActions} from "./action-types";
+import {concatMap, map} from "rxjs/operators";
+import {allCoursesLoaded} from "./course.actions";
 
 @Injectable()
 export class CoursesEffects {
+
+  loadCourses$ = createEffect(
+    () => this.actions$
+      .pipe(
+        ofType(CourseActions.loadAllCourses),
+        // need to return a new action so use concatMap or mergeMap
+        concatMap(action =>
+          // only call backend once
+          this.courseHttpService.findAllCourses()
+        ),
+        map(courses => allCoursesLoaded({courses}))
+      )
+  )
+
+  constructor(private actions$: Actions,
+              private courseHttpService: CoursesHttpService) {
+  }
 
 }
